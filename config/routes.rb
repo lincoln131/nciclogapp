ab4e4eadaba443d1c9ac0f6e69fbae8c469a329c
lincoln131 <lincoln131@gmail.com>
2018-08-13 12:49:31 -0400

    Formatting Changes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   resources :boats
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root "pages#show", page: "about"
+	root "pages#show", page: "index"
 end
