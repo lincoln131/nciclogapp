99cb56423a980c32e80448bad3482b07366b5e8d
lincoln131 <lincoln131@gmail.com>
2018-09-05 11:20:17 -0400

    Fixed some buttons so the users wont be confused

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ApplicationRecord
     
-    include ActiveModel::Dirty
-
-    before_validation :date_format_dash_to_slash, :check_previous_value
+    before_validation :date_format_dash_to_slash
  
     private
         def date_format_dash_to_slash
@@ -10,28 +8,14 @@ class Article < ApplicationRecord
             date_remove.gsub!(/-/,"/") if date_remove.include? "-"
         end
 
-    private
-        def check_previous_value
-        field_names = [oca,operator_name,officer_name,ncic_num,make,model,serial,description,owner_name,owner_phone,date_add,date_remove,remove_by]
-
-        field_names.each do |n|
-
-            
-        end
-
-        end
-
-
     validates :oca,:operator_name,:officer_name,:ncic_num,:make,:model,:serial,:description,:owner_name,:owner_phone,:date_add, presence: true
     
-
     validates :oca, format: {with: /(\A([0-9]{1}[0-9]{1}-{1}[0]{1}[01]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1})\z)|(\A\z)/, message:" - YY-NNNNNN"}
     validates :owner_phone, format: {with: /(\A[2-9]\d{2}-\d{3}-\d{4}\z)|(\A\z)/,message:" - XXX-XXX-XXXX"}
-    
-    
     
     validates :date_add, format: {with: /(\A(1[0-2]|0[1-9]|[1-9])\/(1[0-9]|2[0-9]|3[0-1]|0[1-9]|[1-9])\/\d{4}\z)|(\A\z)/, message:" - MM/DD/YYYY"}
     validates :date_remove, format: {with: /(\A(1[0-2]|0[1-9]|[1-9])\/(1[0-9]|2[0-9]|3[0-1]|0[1-9]|[1-9])\/\d{4}\z)|(\A\z)/, message:" - MM/DD/YYYY"}
 
 
 end
+
