3db4616d871378ee02006c7ed1cced64edfbfcb1
lincoln131 <lincoln131@gmail.com>
2018-09-06 11:41:30 -0400

    Fucking around with fonts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.html { redirect_to @article, notice: 'Article was successfully CREATED.' }
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+        format.html { redirect_to @article, notice: 'Article was successfully UPDATED.' }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to articles_url, notice: 'Article was successfully DELETED!.' }
       format.json { head :no_content }
     end
   end
